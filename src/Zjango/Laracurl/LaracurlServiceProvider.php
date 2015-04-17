9f14335a4d901b0c1d71617e9cb81ee2e68aa4b4
zhu.yuanzhe <zhu.yuanzhe@zhimatech.com>
2015-04-17 23:19:44 +0800

    project init

---
+++ src/Zjango/Laracurl/LaracurlServiceProvider.php
@@ -0,0 +1,46 @@
+<?php namespace Zjango\Laracurl;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaracurlServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['laracurl'] = $this->app->share(function($app){
+			return new Laracurl;
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('laracurl');
+	}
+
+}
